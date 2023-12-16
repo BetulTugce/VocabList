67afa96a972cecc29dc768965884b2e6761625eb
BetulTugce <betultugcedikdogmus@gmail.com>
2023-12-16 13:33:10 +0300

    DbContext yazıldı.

---
+++ VocabList.Repository/Contexts/VocabListDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using VocabList.Core.Entities;
+using VocabList.Core.Entities.Identity;
+
+namespace VocabList.Repository.Contexts
+{
+    public class VocabListDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public VocabListDbContext(DbContextOptions<VocabListDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Sentence> Sentences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configurationları tarayıp uygular..
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
