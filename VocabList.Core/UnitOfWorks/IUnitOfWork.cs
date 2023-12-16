cf18568d958f23686ac47836145f50599a609b4c
BetulTugce <betultugcedikdogmus@gmail.com>
2023-12-16 22:45:46 +0300

    UnitOfWork interfacei ve sınıfı eklendi.

---
+++ VocabList.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace VocabList.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        // Asenkron olarak değişiklikleri kaydeder.
+        Task CommitAsync();
+
+        // Senkron olarak değişiklikleri kaydeder.
+        void Commit();
+    }
+}
---
+++ VocabList.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using VocabList.Core.UnitOfWorks;
+using VocabList.Repository.Contexts;
+
+namespace VocabList.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly VocabListDbContext _context;
+
+        public UnitOfWork(VocabListDbContext context)
+        {
+            _context = context;
+        }
+
+        // Senkron olarak değişiklikleri kaydetmeyi sağlayan metot.
+        public void Commit()
+        {
+            // SaveChanges metodu, veritabanındaki tüm değişiklikleri kalıcı hale getirir.
+            _context.SaveChanges();
+        }
+
+        // Asenkron olarak değişiklikleri kaydetmeyi sağlayan metot.
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
