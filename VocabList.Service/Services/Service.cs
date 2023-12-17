242601ab22bbcb045b6d00b7abccfd311c7ac3c4
BetulTugce <betultugcedikdogmus@gmail.com>
2023-12-17 15:10:29 +0300

    IService interfacei yazıldı ve implement edildi.

---
+++ VocabList.Service/Services/Service.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using VocabList.Core.Repositories;
+using VocabList.Core.Services;
+using VocabList.Core.UnitOfWorks;
+
+namespace VocabList.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        public readonly IGenericRepository<T> _repository;
+        public readonly IUnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _repository.AnyAsync(expression);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<int> GetTotalCountAsync()
+        {
+            return await _repository.GetTotalCountAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _repository.Remove(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            _repository.RemoveRange(entities);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repository.Where(expression);
+        }
+    }
+}
