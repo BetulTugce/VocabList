e5ed0c68339283eee7a8c8ba585333d8e522e7fb
BetulTugce <betultugcedikdogmus@gmail.com>
2023-12-22 23:33:40 +0300

    MudBlazor presentation katmanına dahil edildi.

--- VocabList.UI/Program.cs
+++ VocabList.UI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 using VocabList.UI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
