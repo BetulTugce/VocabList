a4c9f60aab8cb9000dacc3a6f26a47eb08c7d6dd
BetulTugce <betultugcedikdogmus@gmail.com>
2023-12-26 01:48:24 +0300

    Register sayfası hazırlandı ve kullanıcı ekleme işlemleri gerçekleştirildi.

---
+++ VocabList.UI/Data/CreateUserResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace VocabList.UI.Data
+{
+    public class CreateUserResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        // 409 hatası için kullanılacak
+        public bool IsConflictError { get; set; } = false;
+    }
+}
---
+++ VocabList.UI/Services/UserApiService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text;
+using VocabList.UI.Data;
+using static System.Net.WebRequestMethods;
+
+namespace VocabList.UI.Services
+{
+    public class UserApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+
+        public UserApiService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _baseUrl = configuration["ApiSettings:BaseUrl"];
+            //_baseUrl = configuration.GetValue<string>("ApiSettings:BaseUrl");
+        }
+
+        public async Task<CreateUserResponse> CreateUserAsync(CreateUserRequest model)
+        {
+            //İlgili urle verilen model verilerine sahip bir json içeriği gönderiliyor..
+            var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}Users", model);
+            if (response.IsSuccessStatusCode)
+            {
+                ////HTTP yanıtının başarı durum kodu içerip içermediği kontrol edilir..
+                //response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<CreateUserResponse>();
+            }
+            else if (response.StatusCode == HttpStatusCode.Conflict) // HTTP durumu 409 (Conflict) ise
+            {
+                // Hata durumunu kontrol etmek için özel bir nesne döndürür..
+                return new CreateUserResponse { IsConflictError = true };
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
