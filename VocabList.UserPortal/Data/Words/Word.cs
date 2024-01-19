493441a4c9641e6abd3e89b40f07ec9e307dd76e
BetulTugce <betultugcedikdogmus@gmail.com>
2024-01-19 13:12:07 +0300

    Kelimeleri listeleme gerçekleştirildi, ekleme işlemi için dialog penceresi eklendi.

--- VocabList.UserPortal/Data/Words/Word.cs
+++ VocabList.UserPortal/Data/Words/Word.cs
@@ -9,6 +9,8 @@
         public string Description { get; set; }
         public string Type { get; set; }
 
+        public bool ShowDetails { get; set; } = false;
+
         public int WordListId { get; set; }
     }
 }
